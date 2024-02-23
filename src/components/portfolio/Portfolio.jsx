7fd31de0a9f12c1cef64ce28e51766a70fbcab3e
Saurav Gupta <er.sauravgpt@gmail.com>
2024-02-24 00:48:46 +0530

    css improved

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,34 @@
 import { useRef } from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import bikeImg from '../../../public/aboutPic.jpg'
+import skate from '../../../public/skate.jpg'
+import custom from '../../../public/customize.jpg'
+
 
 const items = [
   {
     id: 1,
     title: "About Skate",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    img: bikeImg,
     desc: " Introducing Skater Republic, the thrilling upcoming arcade game that puts you right in the heart of a dynamic skateboarding adventure! Explore expansive skateparks, bustling cityscapes, and renowned real-life locations for an immersive experience like no other. Featuring intuitive controls, a sophisticated trick system, and a compelling progression framework, players will find themselves immersed in a world where every achievement feels truly gratifying. Keep an eye out as we work diligently to bring this extraordinary gaming experience to life.",
-  }
+  },
+  {
+    id: 2,
+    title: "Master Your Skills",
+    img: skate,
+    desc: "  Prepare to embark on an electrifying journey of skill mastery in Skater Republic! From mastering basic flips to conquering gravity-defying aerial maneuvers, the game thrusts you into a high-octane world where every trick is a pulse-pounding thrill. With controls as responsive as your reflexes and physics that defy expectation, each move feels like a heart-pounding victory. Whether you're a rookie or a seasoned pro, Skater Republic promises an adrenaline-fueled ride that will leave you craving more!",
+  },
+
+  {
+    id:3 ,
+    title: "Create and Customize",
+    img: custom,
+    desc: "  Unleash your inner artist and style icon in Skater Republic's boundless realm of customization! Craft your own legendary skateboarder, sculpting every detail from head to toe with a dizzying array of gear, threads, and deck designs. Express yourself like never before as you strut your stuff through the urban jungle, your unique style setting you apart as a true trendsetter. With limitless options to explore, you're not just playing a game – you're living your own skateboarding fantasy!",
+  },
+
+
+
 ];
 
 const Single = ({ item }) => {
@@ -54,8 +74,8 @@ const Portfolio = () => {
   return (
     <div className="portfolio " ref={ref}>
       <div className="progress ">
-        {/* <h1>Featured Works</h1>
-        <motion.div style={{ scaleX }} className="progressBar"></motion.div> */}
+        {/* <h1>Featured Works</h1>*/}
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div> 
       </div>
       {items.map((item) => (
         <Single item={item} key={item.id} />
