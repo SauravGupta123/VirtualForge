15741f5fe96e2a6abeac0365150ece0f19d29bb1
Saurav Gupta <er.sauravgpt@gmail.com>
2024-02-25 19:41:24 +0530

    delay is added for font text

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -48,16 +48,16 @@ const sliderVariants = {
 
 const Hero = () => {
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     const textImgElement = document.querySelector('.textImg');
-  //     if (textImgElement) {
-  //       textImgElement.remove();
-  //     }
-  //   }, 3000); // 48000 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const textImgElement = document.querySelector('.textImg');
+      if (textImgElement) {
+        textImgElement.remove();
+      }
+    }, 48000); // 48000 seconds
 
-  //   return () => clearTimeout(timer);
-  // }, []);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="hero">
       
